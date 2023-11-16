d077b8f16fcb7afa73518233a313c34933296afd
Lukasz Wargocki <lmwargocki@gmail.com>
2023-11-16 19:15:40 +0100

    driver, najbliższe i komentarze; nietestowane

--- rozw.c
+++ rozw.c
@@ -9,58 +9,109 @@ int kandydat_max3(int a, int b, int c);
 int max_int(int a, int b);
 int min_int(int a, int b);
 
-// driver code
+// driver
 int main() {
-  // liczba wierszy i kolumn
-  int rows, cols = 2;
-  // czytaj ilość wierszy z wsadu
-  scanf("%d", &rows);
-  // zbuduj tablicę n
-  int** arr = malloc((long long unsigned int) rows * sizeof(int*));
-  for (int i = 0; i < rows; i++) {
-    arr[i] = malloc((long long unsigned int) cols * sizeof(int));
+  // liczba wierszy i kolumn w tablicy
+  int liczbaHoteli, kolumny = 4;
+
+  // odczytaj ilość wierszy z wejścia
+  scanf("%d", &liczbaHoteli);
+  /* zbuduj dwuwymarową tablicę:
+    w pierwszej kolumnie będzie przechowywać identyfikator sieci
+    w drugiej - odległość od początku trasy - kilometraż
+    w trzeciej indeks najbliższego hotelu innej sieci o niższym indeksie/kilometrażu (hotel A)
+    w czwartej indeks najbliższego hotelu innej sieci o wyższym indeksie/kilometrażu (hotel C)
+  */
+  int **hotele = malloc((size_t) liczbaHoteli * sizeof(int*));
+  for (int i = 0; i < liczbaHoteli; i++) {
+    hotele[i] = malloc((size_t) kolumny * sizeof(int));
   }
-  for (int i = 0; i < rows; i++) {
+
+  // populuj pierwszą kolumnę tablicy identyfikatorami sieci, drugą kolumnę kilometrażami z wejścia
+  for (int i = 0; i < liczbaHoteli; i++) {
     scanf("%d %d", &arr[i][0], &arr[i][1]);
-  };
-    for (int i = 0; i < rows; i++) {
-    printf("%d ", arr[i][0]);
-    printf("%d\n", arr[i][1]);
-  };
-  int min_odleglosc = INT_MAX;
-  int max_odleglosc = -1;
-  for (int i = 0, j = 1, k = 2; i < rows; i++) {
-    if (arr[i][0] == arr[j][0]) { // jeżeli B jest tej samej sieci do A przesuń B i C
-      j++;
-      k++;
-    } else if (arr[i][0] == arr[k][0]) { // jeżeli A i C są tej samej sieci przesuń C
-      k++;
-    } else if (arr[j][0] == arr[k][0]) { // jeżeli B i C są tej samej sieci przesuńC
-      k++;
+  }
+
+  // wstaw indeksy spoza tablicy najbliższej wcześniejszą sieci dla pierwszego hotelu i najbliższej dalszej dla ostatniego
+  hotele[0][2] = -1;
+  hotele[wiersze - 1][3] = wiersze;
+
+  // populuj trzecią kolumnę tablicy - nabliższe wcześniejsze hotele innej sieci
+  // zaczynamy od drugiego i porównujemy z bliższym poprzednikiem
+  for (int i = 1; i < n; i++) {
+      // porównaj sieć obecnego z poprzednim; inne? wpisz indeks poprzedniego te same? wpisz wartość poprzedniego
+      hotele[i][2] = (hotele[i][0] != hotele[i - 1][0]) ? i - 1 : hotele[i - 1][2];
+  }
+
+  // populuj czwartą kolumnę tablicy - najbliższe dalsze hotele innej sieci
+  for (int i = n - 2; i >= 0; i--) {
+    // podobnie jak wyżej, zaczynij od przedostatniego i porównuj z dalszym poprzednikiem
+    hotele[i][3] = (hotele[i][0] != hotele[i + 1][0]) ? i + 1 : hotele[i + 1][3];
+  }
+
+
+
+  // weryfikacja danych z przykładu - WYWAL
+  for (int i = 0; i < liczbaHoteli; i++) {
+    printf("%d ", hotele[i][0]);
+    printf("%d\n", hotele[i][1]);
+    printf("%d\n", hotele[i][2]);
+    printf("%d\n", hotele[i][3]);
+  }
+
+
+
+  // zainicjalizuj zmienne wynikowe z wartościami zawsze zastępowanymi przez funkcje ewaluujące kandydatów
+  int najblizsze3 = INT_MAX;
+  int najdalsze3 = -INT_MAX;
+
+  // szukaj najbliższej trójki hoteli innych sieci, hotel o bieżącym indeksie jest hotelem B w max(B - A, C - B)
+  for (int i = 1; i < wiersze - 1; i++) {
+    // odczytaj indeksy najbliższych sąsiednich hoteli innych sieci - A, B
+    int indeksA = hotele[2][i];
+    int indeksC = hotele[3][i];
+    // nie wykonuj dla indeksów spoza tablicy - wszystkie poprzednie/wszystke dalsze są tej samej sieci co bieżący
+    if (indeksA >= 0 && indeksC < n) {
+      siecA = hotele[indeksA][0];
+      siecC = hotele[indeksC][0];
+      // nie wykonuj jeżeli najbliższy poprzedni jest tej samej sieci co najbliższy dalszy
+      if (siecA != siecC) {
+        // odczytaj odległości od początku, oblicz większy odcinek i zaktualizuj wynik
+        int kilometrA = hotele[indeksA][1];
+        int kilometrC = hotele[indeksC][1];
+        int kilometrB = hotele[i][1];
+        najblizsze3 = min_int(najblizsze3, max_int(kilometrB - kilometrA, kilometrC - kilometrB));
+      }
     }
   }
-  free2DArray(arr, rows);
-  printf("\n7 = %d\n10 = %d\n", min_odleglosc, max_odleglosc);
+  // szukaj najdalszej trójki hoteli innych sieci, hotel o bieżącym indeksie jest hotelem B w min(B - A, C - B)
+  for (int ) {
+    
+  }
+
+  // zwolnij pamięć tablicy
+  free2DArray(hotele, liczbaHoteli);
+
+  // weryfikacja wyniku z przykładu WYWAL
+  printf("\n7 = %d\n10 = %d\n", najblizsze3, najdalsze3);
+
   return 0;
 }
 
-// zwolnij pamięć dwuwymiarowej tablicy integerów o m wierszach
-void free2DArray(int** a, int m) {
-  for (int i = 0; i < m; i++) {
+// pomocnicza funkcja zwalniająca pamięć dwuwymiarowej tablicy integerów o zadanej liczbie wierszy
+void free2DArray(int** a, int wiersze) {
+  for (int i = 0; i < wiersze; i++) {
     free(a[i]);
   }
   free(a);
 }
 
-int kandydat_min3(int a, int b, int c) {
-  return max_int(abs(a - b), abs(b - c));
-}
-int kandydat_max3(int a, int b, int c) {
-  return min_int(abs(a - b), abs(b - c));
-}
+// pomocnicza funkcja -> fmax dla integerów
 int max_int(int a, int b) {
   return a > b ? a : b;
 }
+
+// pomocnicza funkcja -> fmin dla integerów
 int min_int(int a, int b) {
   return a < b ? a : b;
 }
